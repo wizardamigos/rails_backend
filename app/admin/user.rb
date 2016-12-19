71255cd53d72f547565ca0ce0e007b39c82358e1
ninabreznik <ninabreznik@gmail.com>
2016-12-20 07:55:23 +0800

    Update active admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :email, :github_id, :city, :first_name, :last_name, :last_lesson
 #
 # or
 #
@@ -12,6 +12,41 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :github_id
+    column :city
+    column :first_name
+    column :last_name
+    column :created_at
+    column :updated_at
+    column :sign_in_count
+    column :last_lesson
+    actions
+  end
 
+  filter :email
+  filter :github_id
+  filter :city
+  filter :first_name
+  filter :last_name
+  filter :created_at
+  filter :updated_at
+  filter :sign_in_count
+  filter :last_lesson
+
+  form do |f|
+    f.inputs "Edit user" do
+      f.input :email
+      f.input :github_id
+      f.input :city
+      f.input :first_name
+      f.input :last_name
+      f.input :last_lesson
+    end
+    f.actions
+  end
 
 end
--- db/migrate/20151221004944_create_users.rb
+++ db/migrate/20151221004944_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :school
+      t.string :
       t.string :city
 
       t.timestamps null: false
